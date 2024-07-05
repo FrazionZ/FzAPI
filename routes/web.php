ae93cacbe0868314c5ff6e70bb6f4d910b5483c4
= <florent.cala@gmail.com>
2024-07-06 01:59:43 +0200

    Debug

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\UpdaterController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/oue', function () {
+    return response()->json([]);
+});
+
 Route::name('updater.')->prefix('updater')->group(function () {
     Route::get('/oue', function () {
         return response()->json([]);
