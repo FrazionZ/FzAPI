08bd5b8d1c5d6987042f2f4561b54519e7682571
= <florent.cala@gmail.com>
2024-07-06 01:58:10 +0200

    Debug

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,8 @@ use App\Http\Controllers\UpdaterController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('updater.')->prefix('updater')->group(function () {
+    Route::get('/oue', function () {
+        return response()->json([]);
+    });
     Route::get('/check/{target}/{arch}/{current_version}', [UpdaterController::class, 'check'])->name('check');
 });
